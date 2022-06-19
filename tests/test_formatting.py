2a885017c6415c57101202ced400b61ba2e9e985
maxmekiska <maxmekiska@gmail.com>
2022-06-19 18:59:18 +0100

    added test for target data formatting

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -16,17 +16,22 @@ X = np.array([[ 1.10000000e+01,  5.93258427e+00,  1.13483146e+00,
                 1.20000000e+03,  2.77777778e+00,  3.93300000e+01,
                -1.21400000e+02,  3.51790000e+00]])
 
-X = pred_input(to_pred, 2, 'target', 5)[0]
-y = pred_input(to_pred, 2, 'target', 5)[1]
+
+y = np.array([[1.156],
+              [0.983],
+              [1.168],
+              [0.781],
+              [0.771]])
 
 
 class TestFormatting(unittest.TestCase):
 
 
-    def test_pred_input(self):
+    def test_pred_input_X(self):
         np.testing.assert_allclose(pred_input(to_pred, 2, 'target', 5)[0], X)
 
-
+    def test_pred_input_y(self):
+        np.testing.assert_allclose(array(pred_input(to_pred, 2, 'target', 5)[1]), y)
 
 if __name__ == '__main__':
     unittest.main()
