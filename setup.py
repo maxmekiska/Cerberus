2c2fc58ceb07abc0daa832de12300730caf337d4
maxmekiska <maxmekiska@gmail.com>
2022-06-27 23:56:30 +0100

    fixed errors in setup file, cerbus can now be installed via the command line, fixed imports

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     author="Maximilian Mekiska",
     author_email="maxmekiska@gmail.com",
     url="https://github.com/maxmekiska/Cerberus",
     description="Standard and Hybrid Deep Learning Multivariate-Multi-Step & Univariate-Multi-Step Time Series Forecasting.",
-    long_description=open("README.txt").read(),
     name="cerberus",
     version="0.1.0",
-    install_rquires=[
+    packages = find_packages(include=["cerberus", "cerberus.*"]),
+    install_requires=[
         "tensorflow==2.9.1",
         "scikit-learn==0.21.3",
         "matplotlib==3.5.2",
